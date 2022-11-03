0c85237987d4baab99debcfd84f0b933d0b00f11
Anna <Laluna-93@yandex.ru>
2022-11-03 05:51:56 +0400

    feat: added authorization and validation component

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ReactiveFormsModule } from '@angular/forms';
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
+import { LogoutComponent } from './components/logout/logout.component';
+import { LoginComponent } from './components/login/login.component';
+import { ZorroModule } from '../core/zorro/zorro.module';
 
 @NgModule({
   declarations: [
     AuthComponent,
+    LogoutComponent,
+    LoginComponent,
+
   ],
   imports: [
     CommonModule,
     AuthRoutingModule,
+    ZorroModule,
+    ReactiveFormsModule,
   ],
 })
 export class AuthModule { }
---
+++ src/app/auth/components/login/login.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  FormControl, UntypedFormBuilder, UntypedFormGroup, ValidationErrors, Validators,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+  validateForm!: UntypedFormGroup;
+
+  submitForm(): void {
+    if (this.validateForm.valid) {
+      console.log('submit', this.validateForm.value);
+    } else {
+      Object.values(this.validateForm.controls).forEach((control) => {
+        if (control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
+        }
+      });
+    }
+  }
+
+  constructor(private fb: UntypedFormBuilder) {}
+
+  ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      userName: [null, [
+        Validators.required,
+        Validators.email,
+      ]],
+      password: [null, [
+        Validators.required,
+        this.validatorsPassword]],
+    });
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  validatorsPassword(control: FormControl):ValidationErrors | null {
+    const passwordLength = control.value && control.value.length > 7;
+    if (!passwordLength) {
+      return {
+        invalidPassword: 'The password must be at least 8 characters long!',
+      };
+    } if ((!(/[A-Z]/.test(control.value)))) {
+      return {
+        invalidPassword: 'The password must contain uppercase letters!',
+      };
+    }
+    if ((!(/[a-z]/.test(control.value)))) {
+      return {
+        invalidPassword: 'The password must contain lowercase letters!',
+      };
+    }
+
+    if ((!(/[0-9]/.test(control.value)))) {
+      return {
+        invalidPassword: 'The password must contain numbers!',
+      };
+    }
+
+    if ((!(/[!/@,/\]e#?$g%^&.*]/.test(control.value)))) {
+      return {
+        invalidPassword: 'The password must contain characters: e.g., ! @ # ? ]!',
+      };
+    }
+    return null;
+  }
+}
