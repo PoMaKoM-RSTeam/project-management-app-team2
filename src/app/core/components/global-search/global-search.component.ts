c85194e1f7c6186d0106e7d814632c90753fba8d
Kate4ka007 <shlemena@mail.ru>
2022-11-05 22:56:34 +0300

    feat: add header

--- src/app/core/components/global-search/global-search.component.ts
+++ src/app/core/components/global-search/global-search.component.ts
@@ -1,9 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-global-search',
   templateUrl: './global-search.component.html',
   styleUrls: ['./global-search.component.scss'],
 })
-export class GlobalSearchComponent {
+export class GlobalSearchComponent implements OnInit {
+  validateForm!: UntypedFormGroup;
+
+  constructor(private fb: UntypedFormBuilder) { }
+
+  ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      search: [null, []],
+    });
+  }
+
+  submitForm(): void {
+    if (this.validateForm.valid) {
+      console.log('submit', this.validateForm.value);
+    } else {
+      Object.values(this.validateForm.controls).forEach((control) => {
+        if (control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
+        }
+      });
+    }
+  }
 }
--- src/app/localization/localization.component.ts
+++ src/app/localization/localization.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { LocalStorageService } from '../core/services/localstorage.service';
+import { LocalStorageService } from '../core/services/localStorage.service';
 
 @Component({
   selector: 'app-localization',
