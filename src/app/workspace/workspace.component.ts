93b2a6feee7a90b0ed4aa5371439e3fcae4e36ff
Anna <Laluna-93@yandex.ru>
2022-11-11 16:48:50 +0400

    feat: the functionality of receiving and displaying boards from the server has been implemented, styles have been added, a component of creating a board has been added

--- src/app/workspace/workspace.component.ts
+++ src/app/workspace/workspace.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component, OnInit,
+} from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { BoardResponse } from '../core/models/project-manager.model';
 import { ChangeLanguageService } from '../core/services/changeLanguage.service';
+import { HTTPService } from '../core/services/http.service';
+import { CreateBoardService } from './services/create-board-services';
 
 @Component({
   selector: 'app-workspace',
@@ -8,21 +13,36 @@ import { ChangeLanguageService } from '../core/services/changeLanguage.service';
   styleUrls: ['./workspace.component.scss'],
 })
 export class WorkSpaceComponent implements OnInit {
+  isStateFormBoard: boolean | null = null;
+
   nameBoard: string | null = null;
+
+  boards: BoardResponse[] = [];
 
   constructor(
     public translate: TranslateService,
     private languageService: ChangeLanguageService,
+    private createFormService:CreateBoardService,
+    private httpService:HTTPService,
   ) { }
 
   ngOnInit(): void {
     this.languageService.language$.subscribe((value) => this.translate.use(value));
 
     this.nameBoard = 'NAME';
+
+    this.httpService.getAllBoards().subscribe((e) => {
+      this.boards = e;
+      return this.boards;
+    });
   }
 
   // eslint-disable-next-line class-methods-use-this
   deleteBoard() {
+    console.log('1');
+  }
 
+  createForm() {
+    this.createFormService.stateFormBoard(true);
   }
 }
--- src/app/workspace/workspace.module.ts
+++ src/app/workspace/workspace.module.ts
@@ -9,12 +9,14 @@ import { BoardComponent } from './pages/board/board.component';
 import { ColumnComponent } from './components/column/column.component';
 import { LocalizationModule } from '../localization/localization.module';
 import { ZorroModule } from '../core/zorro/zorro.module';
+import { CreateBoardComponent } from './components/create-board/create-board.component';
 
 @NgModule({
   declarations: [
     WorkSpaceComponent,
     ColumnComponent,
     BoardComponent,
+    CreateBoardComponent,
   ],
   imports: [
     CommonModule,
