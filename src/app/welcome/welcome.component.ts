b8e49aa1e4704f4267de7d15e0fd2b5341df62a1
Anna <Laluna-93@yandex.ru>
2022-11-16 15:02:21 +0400

    feat: added the Welcme page and styles

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth/services/auth.service';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.scss'],
 })
-export class WelcomeComponent {
+export class WelcomeComponent implements OnInit {
+  isAuth: boolean | null = null;
+
+  constructor(private isAuthService: AuthService) {}
+
+  ngOnInit() {
+    this.isAuthService.isAuthorized$.subscribe((e) => {
+      this.isAuth = e;
+    });
+  }
 }
