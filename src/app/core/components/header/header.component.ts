af7f5ab4d55bffdbb136a8178cf4dba2fa8c164c
Kate4ka007 <shlemena@mail.ru>
2022-11-22 22:25:32 +0300

    feat: hide search on welcome page

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  showSearch = true;
 
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.showSearch = event.url.includes('workspace');
+      }
+    });
+  }
 }
