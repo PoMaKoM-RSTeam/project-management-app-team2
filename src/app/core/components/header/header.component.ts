41f6b5160d1879a98d6d0bd3bfe472532487e316
Kate4ka007 <shlemena@mail.ru>
2022-11-23 00:06:33 +0300

    fix: styles for navigation links

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
+import { NavigationService } from '../../services/navigation.service';
 
 @Component({
   selector: 'app-header',
@@ -9,12 +10,13 @@ import { Router, NavigationEnd } from '@angular/router';
 export class HeaderComponent implements OnInit {
   showSearch = true;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private navigationService: NavigationService) { }
 
   ngOnInit() {
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
         this.showSearch = event.url.includes('workspace');
+        this.navigationService.updateUrl(event.url);
       }
     });
   }
