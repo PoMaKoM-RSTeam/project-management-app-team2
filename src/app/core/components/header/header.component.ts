19cff7cc322b0629c0ccc00babd2576b900c0c04
Kate4ka007 <shlemena@mail.ru>
2022-11-25 20:34:31 +0300

    feat: add create board button

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { CreateBoardService } from 'src/app/workspace/services/create-board-services';
+import { GetUsersServices } from 'src/app/workspace/services/get-users-services';
+import { HTTPService } from '../../services/http.service';
 import { NavigationService } from '../../services/navigation.service';
 
 @Component({
@@ -10,7 +14,14 @@ import { NavigationService } from '../../services/navigation.service';
 export class HeaderComponent implements OnInit {
   showSearch = true;
 
-  constructor(private router: Router, private navigationService: NavigationService) { }
+  constructor(
+    private router: Router,
+    private navigationService: NavigationService,
+    private createFormService: CreateBoardService,
+    private httpService: HTTPService,
+    private getUsersServices: GetUsersServices,
+    public translate: TranslateService,
+  ) { }
 
   ngOnInit() {
     this.router.events.subscribe((event) => {
@@ -18,6 +29,13 @@ export class HeaderComponent implements OnInit {
         this.showSearch = event.url.includes('workspace');
         this.navigationService.updateUrl(event.url);
       }
+    });
+  }
+
+  createForm() {
+    this.createFormService.stateFormBoard(true);
+    this.httpService.getAllUsers().subscribe((user) => {
+      this.getUsersServices.getUsers(user);
     });
   }
 }
